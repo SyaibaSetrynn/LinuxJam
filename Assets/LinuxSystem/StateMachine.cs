edbf9c8d3ec04ee6e418e0104151f6c15340b30b
SyaibaSetrynn <hoshiya_setrynn@qq.com>
2024-11-19 11:52:07 -0600

    Complete code

--- Assets/LinuxSystem/StateMachine.cs
+++ Assets/LinuxSystem/StateMachine.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class StateMachine : MonoBehaviour
 {
